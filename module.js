cf6b98a686c15b1d32afc3db16119fd3b1ac0531
Kevin Olson <acidjazz@gmail.com>
2020-08-31 00:10:16 -0500

    🎨 readme tweaks, obey config

--- module.js
+++ module.js
@@ -2,13 +2,14 @@ const { resolve } = require('path')
 function toastModule (moduleOptions) {
     const { nuxt } = this
 
-    this.addPlugin({
-        src: resolve(__dirname, 'toast.client.js'),
-        fileName: 'toast.client.js',
-        client: true,
-        mode: 'client',
-        moduleOptions,
-    })
+    if (moduleOptions.toast || moduleOptions.all)
+        this.addPlugin({
+            src: resolve(__dirname, 'toast.client.js'),
+            fileName: 'toast.client.js',
+            client: true,
+            mode: 'client',
+            moduleOptions,
+        })
 }
 module.exports = toastModule
 module.exports.meta = require('./package.json')
