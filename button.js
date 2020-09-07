846ba0abc74ab33fe2ec9d683ca4b48e2ea46e81
Kevin Olson <acidjazz@gmail.com>
2020-09-07 02:13:39 -0500

    ✨ proper plugin install

--- button.js
+++ button.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import { PushButton, ButtonGroup } from 'tv-button'
-import Icons from 'tv-icon'
+import TvButton from 'tv-button'
+import TvIcon from 'tv-icon'
 import 'tv-button/dist/tv-button.css'
-Vue.component('PushButton', PushButton)
-Vue.component('ButtonGroup', ButtonGroup)
-console.log(Icons)
-Vue.use(Icons)
+Vue.use(TvButton)
+Vue.use(TvIcon)
