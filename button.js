a9742fbb05e1fe0566211e494405c1e1e2c10ce8
Kevin Olson <acidjazz@gmail.com>
2020-09-07 01:35:19 -0500

    🚧 1st step at icons

--- button.js
+++ button.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { PushButton } from 'tv-button'
+import { PushButton, ButtonGroup } from 'tv-button'
+import Icons from 'tv-icon'
 import 'tv-button/dist/tv-button.css'
 Vue.component('PushButton', PushButton)
+Vue.component('ButtonGroup', ButtonGroup)
+console.log(Icons)
+Vue.use(Icons)
