54e176898010990ee23e2b2c01752a5a1969498b
kevin olson <acidjazz@gmail.com>
2021-03-19 10:34:34 -0500

    🚧 more TS progress - toast interface separation

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,40 +1,4 @@
-interface NuxtToastInstance {
-    /**
-     * Show a general toast message
-     * @param props
-     */
-    show(props: string|object): void
-
-    /**
-     * Show a successful toast (green)
-     * @param message
-     */
-    success (message: string): void
-
-    /**
-     * Show a general info toast (blue)
-     * @param message
-     */
-    info (message: string): void
-
-    /**
-     * Show a danger-related toast (red)
-     * @param message
-     */
-    danger (message: string): void
-
-    /**
-     * Show a warning-related toast (yellow)
-     * @param message
-     */
-    warning (message: string): void
-
-    /**
-     * Show a denial-related toast (red)
-     * @param message
-     */
-    denied (message: string): void
-}
+import NuxtToastInstance from './toast'
 
 declare module 'vue/types/vue' {
     interface Vue {
