73dafe5fb0c20cce4cba6bef06197e00a0fce665
kevin olson <acidjazz@gmail.com>
2021-04-12 20:05:02 -0500

    🏷️ better types for tv-modal

--- types/modal.d.ts
+++ types/modal.d.ts
@@ -5,7 +5,31 @@ declare interface ModalInterface {
      *
      * @param props
      */
-    show(props: object): void
+    show(props: ModalInterfaceProps): void
+}
+
+declare interface ModalInterfaceProps {
+    /**
+     * Different types show different icons and colors
+     */
+    type: 'success' | 'info' | 'danger' | 'warning'
+    title: string
+    body: string
+    primary: ModalButton
+    secondary: ModalButton
+}
+
+declare interface ModalButton {
+    /**
+     * Button text
+     */
+    label: string
+    /**
+     * Button theme
+     * @link https://github.com/acidjazz/tv-button
+     */
+    theme: 'white' | 'indigo' | 'indigo-light' | 'indigo-dark' | 'red' | 'green'
+    action: Function,
 }
 
 export default ModalInterface
